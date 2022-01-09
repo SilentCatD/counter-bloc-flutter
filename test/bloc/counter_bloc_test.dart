5dbd51150e5f6687eddd8f66a1e7bd52cd07df05
Silent Cat <ngocphuocnguyen312@gmail.com>
2022-01-09 17:59:44 +0700

    write test code for CounterBloc

---
+++ test/bloc/counter_bloc_test.dart
@@ -0,0 +1,38 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:learn_bloc/bloc/counter_bloc.dart';
+
+void main() {
+  group("CounterBloc test", () {
+    late CounterBloc counterBloc;
+    setUp(() {
+      counterBloc = CounterBloc();
+    });
+    tearDown(() {
+      counterBloc.close();
+    });
+
+    test("CounterBLoc initialVal is 0", () {
+      expect(counterBloc.state, const CounterInitial(0));
+    });
+
+    blocTest(
+      'Bloc should emit CounterIncreased() state when IncreaseCounter() event is dispatched',
+      build: () => counterBloc,
+      act: (CounterBloc bloc) {
+        bloc.add(IncreaseCounter());
+      },
+      expect: () => [const CounterIncreased(1)],
+    );
+
+    blocTest(
+      'Bloc should emit CounterDecreased() state when DecreaseCounter() event is dispatched',
+      build: () => counterBloc,
+      act: (CounterBloc bloc) {
+        bloc.add(DecreaseCounter());
+      },
+      expect: () => [const CounterDecreased(-1)],
+    );
+
+  });
+}
